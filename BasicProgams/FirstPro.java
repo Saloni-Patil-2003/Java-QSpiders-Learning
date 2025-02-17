6a99e540c37f95598462ed287816300700eef219
Saloni-Patil-2003 <salonipatil0706@gmail.com>
2025-02-17 13:20:58 +0530

    Added new programs

--- BasicProgams/FirstPro.java
+++
@@ -1,5 +0,0 @@
-class FirstPro{
-public static void main(String[]args){
-System.out.println("Hello World");
-}
-}
--- BasicProgams/Narrowing.java
+++
@@ -1,8 +0,0 @@
-public class Narrowing {
-    public static void main(String[] args) {
-        short a=97;
-        char b=(char)a;
-        System.out.println(b);
-    }
-    
-}
